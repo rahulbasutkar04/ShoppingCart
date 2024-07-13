1dcd4f0460ce2ac47d30e2637de57c5dac45ad6f
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-07-13 17:47:39 +0530

    Updated test case with product creation in domain

---
+++ src/main/java/org/example/shoppingcart/domain/model/entity/Product.java
@@ -0,0 +1,19 @@
+package org.example.shoppingcart.domain.model.entity;
+
+public class Product {
+    private  String  productName;
+    private Double price;
+
+    public Product(String productName, Double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
--- src/test/java/org/example/shoppingcart/ShoppingCartTest.java
+++ src/test/java/org/example/shoppingcart/ShoppingCartTest.java
@@ -1,7 +1,9 @@
 package org.example.shoppingcart;
 
+import org.example.shoppingcart.domain.model.entity.Product;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShoppingCartTest {
@@ -13,5 +15,19 @@ public class ShoppingCartTest {
 
         // assert
         assertTrue(shoppingCart instanceof ShoppingCart);
+    }
+
+    @Test
+    void shouldBeAbleToCreateProductEntity()
+    {
+        // arrange
+        ShoppingCart shoppingCart=new ShoppingCart();
+        Product actualProduct=new Product("Shoes",200.0);
+
+        // act
+        Product expectedProduct=actualProduct;
+
+        // assert
+        assertEquals(actualProduct,expectedProduct);
     }
 }
